b3a8ac4344e9497c77a29bf12f5a40a28be2dce9
Parker Lusk <parkerclusk@gmail.com>
2018-04-13 12:34:04 -0600

    remove need for setting rosparams for the initial body position

--- src/target_motion.cpp
+++ src/target_motion.cpp
@@ -3,11 +3,12 @@
 namespace gazebo
 {
 
-// void TargetMotion::Load(rendering::VisualPtr _parent, sdf::ElementPtr _sdf) {
-
+// void TargetMotion::Load(rendering::VisualPtr _parent, sdf::ElementPtr _sdf)
+// {
 //   visual_ = _parent;
-
 // }
+
+// ----------------------------------------------------------------------------
 
 void TargetMotion::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 {
@@ -49,15 +50,14 @@ void TargetMotion::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
   headingPID_.Init(kpPsi, 0, kdPsi, 0,0, maxVPsi, -maxVPsi);
   altitudePID_.Init(kpZ, 0, kdZ, 0,0, maxVZ, -maxVZ);
 
+  // Capture the initial pose of the mover
+  pose_init_ = model_->GetWorldPose();
 
   // Load trajectory
   loadTrajectory();
 
   // Listen to the update event. This event is broadcast every simulation iteration.
   updateConnection_ = event::Events::ConnectWorldUpdateBegin(boost::bind(&TargetMotion::OnUpdate, this, _1));
-
-  pose_init_ = model_->GetWorldPose();
-
 }
 
 // ------------------------------------------------------------------------
@@ -68,12 +68,8 @@ void TargetMotion::loadTrajectory() {
   // Get ROS parameters
   // 
 
-  // Get initial position
-  float x = nh_.param<float>("x",0);
-  float y = nh_.param<float>("y",0);
-  float z = nh_.param<float>("z",0);
-  move_ = nh_.param<bool>("move_target",true);
-  acceleration_ = nh_.param<float>("acceleration",1);
+  move_ = nh_.param<bool>("move_target", true);
+  acceleration_ = nh_.param<float>("acceleration", 1);
   delta_t_ = 2.4/acceleration_;
 
   // Get relative waypoint lists for x, y and z
@@ -83,12 +79,16 @@ void TargetMotion::loadTrajectory() {
   nh_.getParam("waypoints_z", waypoints_z);
 
   // Construct a waypoint container
-  gzmsg << "waypoints: \n";
-  for (int i=0; i<waypoints_x.size(); i++) {
-    motion::coord_t waypoint (x + waypoints_x[i], y + waypoints_y[i], z + waypoints_z[i]);
-    waypoints_init_.push_back(waypoint); // translate waypoints to origin
+  gzmsg << "waypoints:" << std::endl;
+  for (int i=0; i<waypoints_x.size(); i++)
+  {
+    // translate body-relative waypoints to the world frame
+    motion::coord_t waypoint(pose_init_.pos.x + waypoints_x[i],
+                             pose_init_.pos.y + waypoints_y[i],
+                             pose_init_.pos.z + waypoints_z[i]);
+    waypoints_init_.push_back(waypoint);
 
-    gzmsg << waypoints_init_[i] << "\n";
+    gzmsg << "\t" << waypoints_init_[i] << std::endl;
   }
 
   // trajectory type 
@@ -99,42 +99,32 @@ void TargetMotion::loadTrajectory() {
   params_.lambda = nh_.param<float>("lambda", 1);
 
   // velocity
-  v_ = nh_.param<float>("v",1);
+  v_ = nh_.param<float>("v", 1);
 
   std::vector<std::string> traj_types = {"goToPoint", "waypoints", "circle", "elipse"};
 
 
   // Orbit trajectory
-  if (params_.traj == 2) {
-
+  if (params_.traj == 2)
     follower_ = std::make_shared<motion::Orbit>();
-    gzmsg << "[TargetMotion] Generated a " << traj_types[params_.traj] << " trajectory for " << model_->GetName() << ".\n";
-
-  }
-  else if (params_.traj == 0 || params_.traj == 1 || params_.traj == 3) {
-
+  else if (params_.traj == 0 || params_.traj == 1 || params_.traj == 3)
     follower_ = std::make_shared<motion::StraightLine>();
-    gzmsg << "[TargetMotion] Generated a " << traj_types[params_.traj] << " trajectory for " << model_->GetName() << ".\n";
-    
-
-  }
-  else {
+  else
     gzerr << "[TargetMotion] Trajectory type " << params_.traj << " is undefined.\n";
-  }
+  
+  gzmsg << "[TargetMotion] Generated a " << traj_types[params_.traj] << " trajectory for " << model_->GetName() << ".\n";
 
-  gzmsg << "params: " << "\n k_orbit: " << params_.k_orbit
-                      << "\n row: " << params_.row
-                      << "\n lambda: " << params_.lambda
-                      << "\n k_path: " << params_.k_path
-                      << "\n chi_infinity: " << params_.chi_infinity <<"\n";
+  gzmsg << "params: " << "\n\tk_orbit:      " << params_.k_orbit
+                      << "\n\trow:          " << params_.row
+                      << "\n\tlambda:       " << params_.lambda
+                      << "\n\tk_path:       " << params_.k_path
+                      << "\n\tchi_infinity: " << params_.chi_infinity << std::endl;
 
 
   follower_->set_parameters(params_);
 
   // Set current waypoints
   waypoints_curr_ = waypoints_init_;
-
-
 }
 
 // ------------------------------------------------------------------------
@@ -143,33 +133,31 @@ void TargetMotion::OnUpdate(const common::UpdateInfo& _info)
 {
 
   // calculate the timestep
-   double dt = (simTime_d1_ == 0) ? 0.0001 : _info.simTime.Double() - simTime_d1_;
+  double dt = (simTime_d1_ == 0) ? 0.0001 : _info.simTime.Double() - simTime_d1_;
 
-   if (dt > 1.0/update_rate_ || simTime_d1_ == 0) {
+  if (dt > 1.0/update_rate_ || simTime_d1_ == 0)
+  {  
+   
+    math::Vector3 linear_vel(0, 0, 0); 
+    math::Vector3 angular_vel(0, 0, 0);
 
-   simTime_d1_ = _info.simTime.Double();
-  
- 
-  math::Vector3 linear_vel(0,0,0); 
-  math::Vector3 angular_vel(0,0,0);
+    float chi_er, h_er, yaw, distance;
 
-  float chi_er, h_er, yaw, distance;
+    getCommandError(chi_er, h_er, yaw, distance);
 
-  getCommandError(chi_er, h_er, yaw, distance);
+    getVelCommands(_info, chi_er, h_er, yaw, distance, dt, linear_vel, angular_vel);
+    
 
-  getVelCommands(_info, chi_er, h_er, yaw, distance, dt, linear_vel,angular_vel);
-  
-
-  // Set commands
-  model_->SetLinearVel(linear_vel);
-  model_->SetAngularVel(angular_vel);
+    // Set commands
+    model_->SetLinearVel(linear_vel);
+    model_->SetAngularVel(angular_vel);
 
 
-  PublishState();
+    // capture the current simulation time for next iteration
+    simTime_d1_ = _info.simTime.Double();
 
-   }
-
-
+    PublishState();
+  }
 }
 
 // ------------------------------------------------------------------------
@@ -181,7 +169,7 @@ void TargetMotion::getCommandError(float& chi_er, float h_er, float& yaw, float&
 
   // Get position and pack into type motion::coord_t
   math::Vector3 position = pose.pos;
-  motion::coord_t pos (position.x, position.y, position.z);
+  motion::coord_t pos(position.x, position.y, position.z);
 
   // Get Euler angles
   math::Vector3 rot = pose.rot.GetAsEuler();
@@ -194,17 +182,10 @@ void TargetMotion::getCommandError(float& chi_er, float h_er, float& yaw, float&
 
   // Get heading and altitude commands
   motion::FollowerCommands commands;
-  if (params_.traj == 2) {
-
-    // std::cout << "here" << std::endl;
-    commands = follower_->orbit_follower(waypoints_curr_[0],pos, yaw);
-
-  } 
-  else {
-
-   commands = follower_->line_follower(waypoints_curr_[0], waypoints_curr_[1]-waypoints_curr_[0],pos, yaw);
-
-  }
+  if (params_.traj == 2)
+    commands = follower_->orbit_follower(waypoints_curr_[0], pos, yaw);
+  else
+    commands = follower_->line_follower(waypoints_curr_[0], waypoints_curr_[1]-waypoints_curr_[0], pos, yaw);
 
   // calculate heading and altitude errors
   h_er = std::get<2>(pos) - commands.h_c;
@@ -216,75 +197,70 @@ void TargetMotion::getCommandError(float& chi_er, float h_er, float& yaw, float&
 
 void TargetMotion::getVelCommands(const common::UpdateInfo& _info, double chi_er, double h_er, double yaw, double distance, double dt, math::Vector3& linear_vel, math::Vector3& angular_vel)
 {
-  
- //
- // calculate angular and linear velocity commands
- //
 
- double vz = altitudePID_.Update(h_er, dt);
- double wz = headingPID_.Update(chi_er, dt);
+  //
+  // calculate angular and linear velocity commands
+  //
 
- // If goToPoint trajector and there
- if (params_.traj == 0 && distance < 0.3) {
-  v_ = 0;
-  wz = 0;
-  vz = 0;
- }
+  double vz = altitudePID_.Update(h_er, dt);
+  double wz = headingPID_.Update(chi_er, dt);
 
+  // If goToPoint trajectory and we have arrived, then
+  // trigger the exponential slowing down and stop moving
+  if (params_.traj == 0 && distance < 0.3) move_ = false;
 
- //
- // Rotate commands into the current frame
- //
+  //
+  // Rotate commands into the current frame
+  //
 
- // linear velocity command
- math::Vector3 vel (v_, 0, vz);
+  // linear velocity command
+  math::Vector3 vel (v_, 0, vz);
 
- // Rotation Matrix
- math::Matrix3 rotz(cos(yaw), -sin(yaw), 0.0,
-              sin(yaw),  cos(yaw), 0.0,
-              0.0,       0.0,    1.0);  
+  // Rotation Matrix
+  math::Matrix3 rotz(cos(yaw), -sin(yaw), 0.0,
+                     sin(yaw),  cos(yaw), 0.0,
+                     0.0,       0.0,      1.0);  
 
- // Rotate linear commands into current frame
- linear_vel = rotz*vel;
+  // Rotate linear commands into current frame
+  linear_vel = rotz*vel;
 
- // Get angular vel
- angular_vel = math::Vector3(0,0,wz);
+  // Get angular vel
+  angular_vel = math::Vector3(0, 0, wz);
 
 
- float scale;
- if (move_) {
-  
-  delta_t_ -= dt;
+  float scale;
+  if (move_)
+  {
 
-  scale = std::exp(-delta_t_*acceleration_);
+    delta_t_ -= dt;
 
-  if (scale > 1) {
-   scale = 1;
-   delta_t_ = 0;
+    scale = std::exp(-delta_t_*acceleration_);
+
+    if (scale > 1)
+    {
+      scale = 1;
+      delta_t_ = 0;
+    }
+
+  }
+  else
+  {
+
+    delta_t_ += dt;
+
+    scale = std::exp(-delta_t_*acceleration_);
+
+    if (scale < 0.1)
+    {
+      delta_t_ = 2.4/acceleration_;  
+      scale = 0;
+    }
+
   }
 
- }
- else {
 
-  delta_t_ += dt;
-
-  scale = std::exp(-delta_t_*acceleration_);
-
-  if (scale < 0.1) {
-    delta_t_ = 2.4/acceleration_;  
-    scale = 0;
-  }
-
- }
-
-
- linear_vel = linear_vel*scale;
- angular_vel= angular_vel*scale;
-
-
-
-
-
+  linear_vel = linear_vel*scale;
+  angular_vel = angular_vel*scale;
 }
 
 // ------------------------------------------------------------------------
@@ -315,8 +291,6 @@ void TargetMotion::getVelCommands(const common::UpdateInfo& _info, double chi_er
 //   // dynamic_lines_.Update();
 
 // }
-
-
 
 
 // ------------------------------------------------------------------------
@@ -350,9 +324,6 @@ bool TargetMotion::targetService(moving_targets::MovingTargets::Request &req, mo
     altitudePID_.Reset();
 
     delta_t_ = 2.4/acceleration_;
-
-    // gzmsg << "Reseting " << model_->GetName() << "\n";
-
   }
 
   return true;
@@ -363,26 +334,26 @@ bool TargetMotion::targetService(moving_targets::MovingTargets::Request &req, mo
 
 void TargetMotion::PublishState() {
 
-    // publish mover's state odometry
-    nav_msgs::Odometry odom;
-    odom.header.stamp.sec = model_->GetWorld()->GetSimTime().sec;
-    odom.header.stamp.nsec = model_->GetWorld()->GetSimTime().nsec;
-    odom.header.frame_id = "map";
-    odom.child_frame_id = "target_" + model_->GetName();
-    odom.pose.pose.position.x     = model_->GetWorldPose().pos.x;
-    odom.pose.pose.position.y     = model_->GetWorldPose().pos.y;
-    odom.pose.pose.position.z     = model_->GetWorldPose().pos.z;
-    odom.pose.pose.orientation.w  = model_->GetWorldPose().rot.w;
-    odom.pose.pose.orientation.x  = model_->GetWorldPose().rot.x;
-    odom.pose.pose.orientation.y  = model_->GetWorldPose().rot.y;
-    odom.pose.pose.orientation.z  = model_->GetWorldPose().rot.z;
-    odom.twist.twist.linear.x     = model_->GetRelativeLinearVel().x;
-    odom.twist.twist.linear.y     = model_->GetRelativeLinearVel().y;
-    odom.twist.twist.linear.z     = model_->GetRelativeLinearVel().z;
-    odom.twist.twist.angular.x    = model_->GetRelativeAngularVel().x;
-    odom.twist.twist.angular.y    = model_->GetRelativeAngularVel().y;
-    odom.twist.twist.angular.z    = model_->GetRelativeAngularVel().z;
-    state_.publish(odom);
+  // publish mover's state odometry
+  nav_msgs::Odometry odom;
+  odom.header.stamp.sec = model_->GetWorld()->GetSimTime().sec;
+  odom.header.stamp.nsec = model_->GetWorld()->GetSimTime().nsec;
+  odom.header.frame_id = "map";
+  odom.child_frame_id = "target_" + model_->GetName();
+  odom.pose.pose.position.x     = model_->GetWorldPose().pos.x;
+  odom.pose.pose.position.y     = model_->GetWorldPose().pos.y;
+  odom.pose.pose.position.z     = model_->GetWorldPose().pos.z;
+  odom.pose.pose.orientation.w  = model_->GetWorldPose().rot.w;
+  odom.pose.pose.orientation.x  = model_->GetWorldPose().rot.x;
+  odom.pose.pose.orientation.y  = model_->GetWorldPose().rot.y;
+  odom.pose.pose.orientation.z  = model_->GetWorldPose().rot.z;
+  odom.twist.twist.linear.x     = model_->GetRelativeLinearVel().x;
+  odom.twist.twist.linear.y     = model_->GetRelativeLinearVel().y;
+  odom.twist.twist.linear.z     = model_->GetRelativeLinearVel().z;
+  odom.twist.twist.angular.x    = model_->GetRelativeAngularVel().x;
+  odom.twist.twist.angular.y    = model_->GetRelativeAngularVel().y;
+  odom.twist.twist.angular.z    = model_->GetRelativeAngularVel().z;
+  state_.publish(odom);
 }
 
 }
